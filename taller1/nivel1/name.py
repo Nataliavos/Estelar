6de7842aaf1451ae853e20a21fffa1e6e42a30fa
Nataliav <nataliav0517@gmail.com>
2025-11-06 12:08:49 -0500

    1. Hola usuario

---
+++ taller1/nivel1/name.py
@@ -0,0 +1,5 @@
+'''Hola usuario: pide al usuario su nombre y edad. Luego imprime un mensaje: "Hola [nombre], tienes [edad] años."'''
+name = input("Ingrese su nombre:")
+age = int(input("Ingrese su edad:"))
+
+print(f"Hola {name}, tienes {age} años.")

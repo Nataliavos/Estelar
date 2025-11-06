dc5263841eff686cbc252570dbec66294db3903b
Natalia <nataliav0517@gmail.com>
2025-11-06 17:45:17 -0500

    7. Mayor de edad

---
+++ taller1/level_2/legal_age.py
@@ -0,0 +1,8 @@
+#Mayor de edad
+
+age = int(input("Ingrese su edad: "))
+
+if age >= 18:
+    print(f"Tienes {age} años, eres mayor de edad.")
+else:
+    print(f"Tienes {age} años, eres menor de edad.")

8d295fd7bd393d06ad0847f0949e2ec4dd4dd847
Nataliav <nataliav0517@gmail.com>
2025-11-06 13:57:05 -0500

    6. Edad futura

---
+++ taller1/level_1/future_age.py
@@ -0,0 +1,6 @@
+'''Edad futura: pide la edad actual y muestra cuántos años tendrá el usuario dentro de 10 años.'''
+age = int(input("Ingrese su edad actual: "))
+in_years = 10
+future_age = age + in_years
+
+print(f"Actualmente tienes: {age} años. En {in_years} años tendrás: {future_age} años")

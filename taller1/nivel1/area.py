4dfed3064371ab8700eccb934c0240f67267b7d2
Nataliav <nataliav0517@gmail.com>
2025-11-06 12:56:45 -0500

    3. area del triangulo

---
+++ taller1/nivel1/area.py
@@ -0,0 +1,8 @@
+'''Área del triángulo.'''
+
+base = float(input("Ingresa la base: "))
+height = float(input("Ingresa la altura: "))
+
+area = (base * height) / 2
+
+print(f"El área de triángulo es: {area}")

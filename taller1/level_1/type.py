5b0c328e64480aaf3886ccbf6086352c6fbc7f96
Nataliav <nataliav0517@gmail.com>
2025-11-06 13:49:32 -0500

    5. Tipo de dato type()

---
+++ taller1/level_1/type.py
@@ -0,0 +1,20 @@
+'''Tipo de dato: usar type() para mostrar el tipo de cada variable.'''
+
+name = "Natalia"
+mess_name = f"La variable (name) es igual a ({name}) y su tipo es ({type(name).__name__})"
+print(mess_name)
+
+
+age = 27
+mess_age = f"La variable (age) es igual a ({age}) y su tipo es ({type(age).__name__})"
+print(mess_age)
+
+height = 1.55
+mess_height = f"La variable (height) es igual a ({height}) y su tipo es ({type(height).__name__})"
+print(mess_height)
+
+female = True
+mess_female = f"La variable (female) es igual a ({female}) y su tipo es ({type(female).__name__})"
+print(mess_female)
+
+

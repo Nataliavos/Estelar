f0370ff078a88b4285c4c91142018acf7bd74076
veronicahrnndz08 <108330996+veronicahrnndz08@users.noreply.github.com>
2025-11-07 00:33:35 -0500

    commit_1

---
+++ Nivel 1 — Fundamentos y Variables.py
@@ -0,0 +1,7 @@
+nombre = input("Por favor, introduce tu nombre: ")
+print("¡Hola, " + nombre + "!")
+
+edad = input("Por favor, introduce tu edad: ")
+print("Tienes " + edad + " años.")
+
+print("Hola " + nombre + ", tienes " + edad + " años.")
